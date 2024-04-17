8ed0f29b490489d843f91db80f7c5d6271d4936a
Justyna Sikorska <justynasikorska15@gmail.com>
2024-04-17 18:29:27 +0200

    simple changes

--- battleships/src/BattleGame.tsx
+++ battleships/src/BattleGame.tsx
@@ -8,41 +8,21 @@ import './BattleGame_style.css';
 //   );
 // }
 
-function Square({value=""}){
+function Square({value="", field=""}){
   return (
-    <button className='butt'>
+    <button className='butt' id={field}>
       {value}
     </button>
     );
 }
 
 function FewSquares({row}: {row:number}){
-  return (
-    <div id="test">
-      <button id="pTest">{row}</button> 
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    
-  );
-}
+  let final_row = "";
 
-function test() {
-  for(var i = 0; i < 5; i++){
-    <button>i</button>
-  }
-}
-
-function Letters(){ 
-  return (
+  if(row){
+    final_row = row.toString();
+  }else{
+    return (
     <div>
       <Square />
       <Square value='A'/>
@@ -54,7 +34,44 @@ function Letters(){
       <Square value='G'/>
       <Square value='H'/>
       <Square value='I'/>
-      <Square value='J'/>
+      <Square value='J'/> 
+    </div>
+    )
+  }
+
+  return (
+    <div id="test">
+      <button id="row">{final_row}</button> 
+      <Square field={"A" + final_row}/>
+      <Square field={"B" + final_row}/>
+      <Square field={"C" + final_row}/>
+      <Square field={"D" + final_row}/>
+      <Square field={"E" + final_row}/>
+      <Square field={"F" + final_row}/>
+      <Square field={"G" + final_row}/>
+      <Square field={"H" + final_row}/>
+      <Square field={"I" + final_row}/>
+      <Square field={"J" + final_row}/>
+    </div>
+    
+  );
+}
+
+function Letters(){ 
+  return (
+    <div>
+      {/* <Square />
+      <Square value='A'/>
+      <Square value='B'/>
+      <Square value='C'/>
+      <Square value='D'/>
+      <Square value='E'/>
+      <Square value='F'/>
+      <Square value='G'/>
+      <Square value='H'/>
+      <Square value='I'/>
+      <Square value='J'/> */}
+      <FewSquares row={0} />
   </div>
   );
 }
@@ -84,7 +101,7 @@ function Game() {
     <div className="Game">
       <header className="Battleships Game">
       
-        <table id="upper-row">
+        {/* <table id="upper-row">
           <td>A</td> <td>B</td> <td>C</td> <td>D</td> <td>E</td> <td>F</td>
           <td>G</td> <td>H</td> <td>I</td> <td>J</td>
         </table>
@@ -93,7 +110,7 @@ function Game() {
                 <tr><td>1</td></tr> <tr><td>2</td></tr> <tr><td>3</td></tr> <tr><td>4</td></tr> <tr><td>5</td></tr>
                 <tr><td>6</td></tr> <tr><td>7</td></tr> <tr><td>8</td></tr> <tr><td>9</td></tr> <tr><td>10</td></tr>
             </table>
-        </div>
+        </div> */}
         <div >
             <Board  />
         </div>
