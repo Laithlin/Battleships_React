54a04daac7430d4f19ab758b8f624e34297997df
Justyna Sikorska <justynasikorska15@gmail.com>
2024-04-20 17:28:51 +0200

    added logo

--- battleships/src/components/Menu.tsx
+++ battleships/src/components/Menu.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "../styles/Menu.css"
+import logo from '../assets/logo.png'
 
 function Menu() {
     return (
-        <div className="menu">
-            <button className="menu_button" id="single">
-                <a href="/singlePlayer" style={{color: "#5fc9f3"}}>Single player</a>
-            </button>
-            <button className="menu_button" id="multi">
-                Multiplayer
-            </button>
-            <button className="menu_button" id="settings">
-                Settings
-            </button>
+        <div id="menu_title">
+            <img src={logo} alt="logo" id="logo"/>
+            <div className="menu">
+                <a href="/singlePlayer" >
+                    <button className="menu_button" id="single">Single player</button>
+                </a>
+                <a href="/multiplayer">
+                    <button className="menu_button" id="multi">Multiplayer</button>
+                </a>
+                <a href="/settings">
+                    <button className="menu_button" id="settings">Settings</button>
+                </a>
+            </div>
         </div>
+        
     );
 }
 
