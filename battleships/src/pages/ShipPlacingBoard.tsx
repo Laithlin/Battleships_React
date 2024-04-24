6595f8cfc954471b203bf60111505570cb26fcef
Justyna Sikorska <justynasikorska15@gmail.com>
2024-04-24 22:39:56 +0200

    assets + rotate one element

--- battleships/src/pages/ShipPlacingBoard.tsx
+++ battleships/src/pages/ShipPlacingBoard.tsx
@@ -1,12 +1,27 @@
+import React, {useState} from "react";
 import Board from "../components/Board";
 import "../styles/ShipPlacingBoard_style.css"
+import Battleship from "../assets/ShipBattleshipHull.png"
+import Destroyer from "../assets/ShipDestroyerHull.png"
+import Patrol from "../assets/ShipPatrolHull.png"
+import Submarine from "../assets/ShipSubMarineHull.png"
 
 function ShipPlacingBoard() {
+    const[style, setRotate] = useState("not-rotated");
+    const rotate = () => {
+        console.log("click");
+        if (style === "not-rotated") setRotate("rotated");
+            else setRotate("not-rotated");
+    };
     return (
         <div>
             <div id="placing_ships">
                 <Board id="placing_board"/>
                 <div id="elements_table">
+                    <img src={Destroyer} alt="Destroyer" />
+                    <img src={Battleship} alt="Battleship" />
+                    <img src={Patrol} alt="Patrol" />
+                    <img src={Submarine} alt="Submarine" className={style} onDoubleClick={rotate}/>
 
                 </div>
             </div>
